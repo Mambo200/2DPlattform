9693279f36b295b738c25a33966c4592b94c6c76
Tobias@Home <tobis-stroedicke@gmx.de>
2020-03-17 16:04:47 +0100

    Add levelmanager Add Cameracontroller

---
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : LevelManagers
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+}
