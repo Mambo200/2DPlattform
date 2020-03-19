737f70560926a8a4a8777d755df5c337e7b64c69
Tobias@Home <tobis-stroedicke@gmx.de>
2020-03-19 12:25:36 +0100

    Fix Jump and Dash

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 
 public class PlayerMover : MainScript
 {
+    /// <summary>
+    /// Rigidbody of this Object
+    /// </summary>
     private Rigidbody RBody { get; set; }
+
+    /// <summary>
+    /// Collider of this Object
+    /// </summary>
     private Collider Collider { get; set; }
 
     public float m_MovementSpeed;
     public float m_JumpSpeed;
+
+    [Header("Dash Variables")]
+    public bool m_DashAllowed;
+    public float m_DashSpeedMultiplier;
+    public float m_DashDuration;
+
+    [SerializeField]
+    private bool m_JumpInAirWhileDash;
+    private bool jumpedInAir;
+
+    public bool InDash { get; private set; }
+
+    private float tempDashTime;
+
+    /// <summary>
+    /// true if player is grounded. should be set at the beginning of Update Function
+    /// </summary>
+    public bool Grounded { get; private set; }
 
     // Start is called before the first frame update
     public override void Start()
@@ -22,52 +47,102 @@ public class PlayerMover : MainScript
     // Update is called once per frame
     public override void Update()
     {
-        float movement = GetMovement() * m_MovementSpeed;
-        Vector3 toMove = Jump(movement);
+        Grounded = IsGrounded();
+        Vector3 toMove = new Vector3(
+            0, 
+            0,
+            GetMovement(m_DashAllowed) * m_MovementSpeed
+            );
         Move(toMove);
-        Debug.Log(RBody.velocity);
+        Jump();
+        VelocityCheck();
     }
 
+    /// <summary>
+    /// Moves the Player to x, y and z Axis.
+    /// </summary>
+    /// <param name="_direction">Direction to Move</param>
     private void Move(Vector3 _direction)
     {
         Vector3 velocity = _direction;
-        velocity.y = RBody.velocity.y;
+        velocity.y += RBody.velocity.y;
         RBody.velocity = velocity;
     }
 
-    private float GetMovement() => Input.GetAxisRaw("Horizontal");
-
-    private Vector3 Jump(float _movement)
+    /// <summary>
+    /// Get horizontal Input (A and D) and calculate with Dash
+    /// </summary>
+    /// <returns>Horizontal input</returns>
+    private float GetMovement(bool _dashAllowed)
     {
-        bool isGrounded = IsGrounded();
-        Vector3 toReturn = new Vector3(0, 0, _movement);
-        if (Input.GetButton("Jump") && isGrounded)
-        {
-            //RBody.AddForce(
-            //    Vector3.up * m_JumpSpeed,
-            //    ForceMode.VelocityChange
-            //    );
-            RBody.AddExplosionForce(
-                m_JumpSpeed,
-                this.transform.position - new Vector3(0,Collider.bounds.extents.y, 0),
-                3f,
-                10f,
-                ForceMode.VelocityChange
-                );
+        float toReturn = Input.GetAxisRaw("Horizontal");
+        if (_dashAllowed) toReturn = GetDash(toReturn);
+        return toReturn;
+    } 
 
-            if (RBody.velocity.y >= 10)
+    /// <summary>
+    /// Get horizontal Dash.
+    /// </summary>
+    /// <param name="_currentMovement">current Movement of Player</param>
+    /// <returns></returns>
+    private float GetDash(float _currentMovement)
+    {
+        if(!InDash && _currentMovement != 0  && Input.GetAxisRaw("Dash") != 0)
+        {
+            _currentMovement *= m_DashSpeedMultiplier;
+            tempDashTime = m_DashDuration;
+            InDash = true;
+            DashBeginn();
+        }
+        else if (InDash)
+        {
+            _currentMovement *= m_DashSpeedMultiplier;
+            tempDashTime -= Time.deltaTime;
+            DashStay();
+            if (tempDashTime <= 0 && Grounded)
+            {
+                InDash = false;
+                jumpedInAir = false;
+                DashEnd();
+            }
+        }
+
+        return _currentMovement;
+    }
+
+    /// <summary>
+    /// Add Jump velocity if Jump key is pressed
+    /// </summary>
+    private void Jump()
+    {
+        if(Grounded || (InDash && m_JumpInAirWhileDash && !jumpedInAir))
+            if (Input.GetButton("Jump"))
+            {
                 RBody.velocity = new Vector3(
                     RBody.velocity.x,
-                    10,
+                    m_JumpSpeed,
                     RBody.velocity.z
                     );
 
-            Debug.Log(this.transform.position - new Vector3(0, (this.transform.position.y - Collider.bounds.extents.y), 0));
+                if (!jumpedInAir) DashJump();
+                jumpedInAir = true;
+            }
+    }
+
+    /// <summary>
+    /// Check if Velocity is higher than is should be
+    /// </summary>
+    private void VelocityCheck()
+    {
+        if (RBody.velocity.y >= 10)
+        {
+
+            RBody.velocity = new Vector3(
+                RBody.velocity.x,
+                10,
+                RBody.velocity.z
+                );
         }
-
-
-
-        return toReturn;
     }
 
     private void OnCollisionStay(Collision collision)
@@ -75,6 +150,9 @@ public class PlayerMover : MainScript
 
     }
 
+    /// <summary>
+    /// Check if Player is on the ground
+    /// </summary>
     private bool IsGrounded()
     {
         return Physics.Raycast(
@@ -83,4 +161,31 @@ public class PlayerMover : MainScript
             (Collider.bounds.extents.y + 0.1f)
             );
     }
+
+
+
+
+
+    #region Dash Methods
+    private void DashBeginn()
+    {
+        Debug.Log("Dash Begin");
+    }
+
+    private void DashStay()
+    {
+        Debug.Log("Dash Stay");
+    }
+
+    private void DashJump()
+    {
+        Debug.Log("Dash Jump");
+    }
+
+    private void DashEnd()
+    {
+        Debug.Log("Dash End");
+    }
+
+    #endregion
 }
